d450602c68dd8c0dfcbd452a92fb6dbd113342db
miller-cano <73566526+miller-cano@users.noreply.github.com>
2020-12-08 15:14:51 -0500

    Add files via upload

--- ClaseCola.h
+++ ClaseCola.h
@@ -1,4 +1,6 @@
 #include <iostream>
+#include "ClasePila.h"
+#include "ClaseListaCircular.h"
 
 using namespace std;
 
@@ -15,16 +17,15 @@ class Cola{
 		int GetTope();
 		void SetTope(int t);
 		int GetVector();
-		int GetVectorTope();
 		void SetVector(int i);
-		void SetVectorTope(int i);
 		void Encolar(int d);
 		int Desencolar();
 		bool ColaLlena();
 		bool ColaVacia();
 		void LlenarCola(Cola A);
 		void Mostrar();
-		void InvertirCola(); 
+		void Convertir_Lista(Lista_C *);
+		void Convertir_Pila(Pila A);
 		void IntercalarColas(Cola A, Cola B);
 		~Cola();
 };
@@ -61,21 +62,10 @@ int Cola::GetVector()
 	return vector[0];
 }
 
-int Cola::GetVectorTope()
-{
-	return vector[tope];
-}
-
 void Cola::SetVector(int i)
 {
 	vector[0] = i;
-}
-
-void Cola::SetVectorTope(int i)
-{
-	vector[tope] = i;
-}
-		
+}	
 		
 bool Cola::ColaLlena()
 {
@@ -97,13 +87,11 @@ bool Cola::ColaVacia()
 
 void Cola::Encolar(int d)
 {
-	bool llena;
-	llena = ColaLlena();
-	if(llena == false){
+	if(ColaLlena() == false){
 		tope++;
 		vector[tope] = d;
  	}
-		else if(llena == true){
+		else if(ColaLlena() == true){
 		//	cout<<"\nLA COLA ESTA LLENA"<<endl;
 		}
 }
@@ -111,9 +99,8 @@ void Cola::Encolar(int d)
 int Cola::Desencolar()
 {
 	int d, i = 1;
-	bool vacia;
-	vacia = ColaVacia();
-	if(vacia == false){
+	
+	if(ColaVacia() == false){
 		d = vector[0];
 		while(i <= tope){
 			vector[i-1] = vector[i];
@@ -121,7 +108,7 @@ int Cola::Desencolar()
 		}
 		tope--;
 	}
-		else if(vacia == true){
+		else if(ColaVacia() == true){
 		//	cout<<"\nLA COLA ESTA VACIA"<<endl;
 		}
 	return d;
@@ -140,69 +127,62 @@ void Cola::Mostrar()
 	Cola aux(limite+1);
 	while(ColaVacia() == false){
 		t = Desencolar();
-		cout<<"| "<<t<<" |"<<endl;
-		cout<<"------"<<endl;
+		cout<<"| "<<t<<" |";
 		aux.Encolar(t);
 	}
 	LlenarCola(aux);
-	cout<<"\n";
+	cout<<"\n\n";
 }
 
-void Cola::InvertirCola()
+void Cola::Convertir_Lista(Lista_C *A){
+	while(ColaLlena() == false){
+		Encolar(A->sacarDato());
+		A->mostrarLista();
+	}
+}
+
+void Cola::Convertir_Pila(Pila A)
 {
-	int v;
-	Cola aux(limite+1), aux2(limite+1);
-	while(ColaVacia() == false){
-		v = Desencolar();
-		aux.Apilar(v);
+	if(A.PilaVacia() == false){	
+		while(A.PilaVacia() == false){
+			Encolar(A.Desapilar());
+		}
 	}
-	while(aux.ColaVacia() == false){
-		aux2.Encolar(aux.Desapilar());
+	else{
+		cout<<"LA PILA ESTA VACIA";
 	}
-	LlenarCola(aux2);
-	cout<<"\nCOLA INVERTIDA"<<endl;
 }
 
-void Cola::IntercalarColas (Cola A, Cola B){
-	
+void Cola::IntercalarColas (Cola A, Cola B)
+{
 	Cola AuxA(A.GetLimite()+1);
 	Cola AuxB(B.GetLimite()+1);
-	int r,d;
-	
-	
-	if (A.ColaVacia() == false && B.ColaVacia() == false){
-		
-		while (A.ColaVacia() == false && B.ColaVacia() == false){
-		
+	int r,d;	
+	if (A.ColaVacia() == false || B.ColaVacia() == false){
+		while (A.ColaVacia() == false && B.ColaVacia() == false){		
 			r = A.Desencolar();
 			d = B.Desencolar();	
 			Encolar(r);
 			Encolar(d);
 			AuxA.Encolar(r);
 			AuxB.Encolar(d);
-			
 		}
-		
-		}else if (A.ColaVacia() == false){
-		
-			while(A.ColaVacia() == false){
-		
+	
+		while(A.ColaVacia() == false){
 			r = A.Desencolar();
 			Encolar(r);
 		}
-			}else if(B.ColaVacia() == false){
-			
-				while(B.ColaVacia() == false){
 		
-				d = B.Desencolar();
-				Encolar(d);
-				}
-			}else cout<<"\nProceso Terminado";
-	
-	
-	A.LlenarCola(AuxA);
-	B.LlenarCola(AuxB);
-	
-
+		while(B.ColaVacia() == false){
+			d = B.Desencolar();
+			Encolar(d);
+		}
+		A.LlenarCola(AuxA);
+		B.LlenarCola(AuxB);
+	}
+	else{
+		cout<<"\nLAS COLAS ESTAN VACIAS"<<endl;
+	}
 }
+
 Cola::~Cola() { }
